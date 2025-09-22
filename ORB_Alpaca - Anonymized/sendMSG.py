b19fbe92abd87a2a2704d877cf913be467f0934a
maxmittag <76822215+maxmittag@users.noreply.github.com>
2025-09-22 12:53:39 +0200

    Update sendMSG.py

--- ORB_Alpaca - Anonymized/sendMSG.py
+++ ORB_Alpaca - Anonymized/sendMSG.py
@@ -1,6 +1,7 @@
 import requests
 
 def sendMessage(input_message):
-    base_url = "https://api.telegram.org/bot6754066340:AAHRU8ai0qhSo9r3tPUNu8NkhALzBUYgRu4/sendMessage?chat_id=-SECRET&text={}".format(input_message)
+    base_url = "https://api.telegram.org/botSECRET/sendMessage?chat_id=-SECRET&text={}".format(input_message)
     requests.get(base_url)
     return
+
